61568d9c991b7e7ca7d05efd75a450b84eafe007
PepePatronum <81338165+JRound00@users.noreply.github.com>
2024-03-16 22:05:16 +0000

    delete all

--- README.md
+++
@@ -1,3 +0,0 @@
-https://jround00.github.io/portfolio/ 
-
-This is a quick website I will be using throughout my career as a cyber security consultant.
--- script.js
+++
@@ -1,7 +0,0 @@
-// Dynamically show descriptions on hover
-document.querySelectorAll('.walkthrough-info').forEach(walkthrough => {
-    const description = walkthrough.dataset.description;
-    const descriptionElement = document.createElement('p');
-    descriptionElement.textContent = description;
-    walkthrough.appendChild(descriptionElement);
-});
